da6392a7ca65599e950542dc59e62ea3713947f1
Rasmus Dahlberg <rasmus.dahlberg@kau.se>
2020-11-05 17:19:55 +0100

    allowed client configuration without certificate chain

--- client/get-sth/main.go
+++ client/get-sth/main.go
@@ -14,13 +14,12 @@ import (
 var (
 	operators = flag.String("operators", "../../descriptor/stfe.json", "path to json-encoded list of log operators")
 	logId     = flag.String("log_id", "B9oCJk4XIOMXba8dBM5yUj+NLtqTE6xHwbvR9dYkHPM=", "base64-encoded log identifier")
-	chain     = flag.String("chain", "../../server/testdata/x509/end-entity.pem", "path to pem-encoded certificate chain that the log accepts")
 )
 
 func main() {
 	flag.Parse()
 
-	client, err := client.NewClientFromPath(*logId, *chain, "", *operators, &http.Client{}, true)
+	client, err := client.NewClientFromPath(*logId, "", "", *operators, &http.Client{}, true)
 	if err != nil {
 		glog.Fatal(err)
 	}
