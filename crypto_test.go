de550a6665cdbc295878f94a283b94b1e3368e6d
Rasmus Dahlberg <rasmus.dahlberg@kau.se>
2020-11-17 18:06:47 +0100

    added test placeholders

---
+++ crypto_test.go
@@ -0,0 +1,21 @@
+package stfe
+
+import (
+	"testing"
+)
+
+// TODO: TestBuildChainFromDerList
+func TestBuildChainFromDerList(t *testing.T) {
+}
+
+// TODO: TestVerifySignature
+func TestVerifySignature(t *testing.T) {
+}
+
+// TODO: TestGenV1Sdi
+func TestGenV1Sdi(t *testing.T) {
+}
+
+// TODO: TestGenV1Sth
+func TestGenV1Sth(t *tesing.T) {
+}
---
+++ reqres_tests.go
@@ -0,0 +1,29 @@
+package stfe
+
+import (
+	"testing"
+)
+
+// TODO: TestNewAddEntryRequest
+func TestNewAddEntryRequest(t *testing.T) {
+}
+
+// TODO: TestNewGetEntriesRequest
+func TestNewGetEntriesRequest(t *testing.T) {
+}
+
+// TODO: TestNewGetProofByHashRequest
+func TestNewGetProofByHashRequest(t *testing.T) {
+}
+
+// TODO: TestNewGetConsistencyProofRequest
+func TestNewGetConsistencyProofRequest(t *testing.T) {
+}
+
+// TODO: TestNewGetEntryResponse
+func TestNewGetEntriesResponse(t *testing.T) {
+}
+
+// TODO: TestNewGetAnchorsResponse
+func TestNewGetAnchorsResponse(t *testing.T) {
+}
