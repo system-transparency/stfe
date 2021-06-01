bd7f85f538a14e59efd647c3fadf9e6dbc6383f7
Rasmus Dahlberg <rasmus.dahlberg@kau.se>
2021-06-02 00:40:31 +0200

    added leaf hash helper

--- types/util.go
+++ types/util.go
@@ -4,10 +4,18 @@ import (
 	"crypto/sha256"
 )
 
+const (
+	LeafHashPrefix = 0x00
+)
+
 func Hash(buf []byte) *[HashSize]byte {
 	var ret [HashSize]byte
 	hash := sha256.New()
 	hash.Write(buf)
 	copy(ret[:], hash.Sum(nil))
 	return &ret
+}
+
+func HashLeaf(buf []byte) *[HashSize]byte {
+	return Hash(append([]byte{LeafHashPrefix}, buf...))
 }
